84a97cc0375915b263b25931e1829cb1c946c01f
Gregory Valderrama <gvalderrama@verizon.com>
2017-10-08 18:26:17 -0500

    add

---
+++ aynione.website/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace aynione.website
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try{
+                BuildWebHost(args).Run();
+			}
+            catch( Exception ex ){
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                   .UseStartup<Startup>().UseUrls("http://0.0.0.0:50002")
+                .Build();
+    }
+}
