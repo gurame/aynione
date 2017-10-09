b39939725dd003e5715c15ca068037be7b1a8a38
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2017-10-09 00:48:39 -0500

    add

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # ayni one
+
+# Requirements
+
+Install Nuget Command Line 
+
+https://chocolatey.org/packages/NuGet.CommandLine
+
 [![Build status](https://ci.appveyor.com/api/projects/status/42s1xul28amnxe9j?svg=true)](https://ci.appveyor.com/project/gcvalderrama/aynione)
 
--- RunUnitTest.ps1
+++ RunUnitTest.ps1
@@ -1,6 +1,8 @@
+nuget restore ./packages.tools.config -OutputDirectory ./Packages
 dotnet build -c  Debug
 OpenCover.Console.exe -target:".\dotnet.exe" -targetargs:"test ./tests/aynione.unittest/aynione.unittest.csproj" -register:user -filter:"+[aynione*]*" -oldStyle -output:./coverage.xml 
 $coveralls = (Resolve-Path "./Packages/coveralls.net.*/tools/csmacnz.coveralls.exe").ToString()
+$reportgenerator = (Resolve-Path "./Packages/ReportGenerator.*/tools/ReportGenerator.exe").ToString()
 write-host "repotoken" $env:COVERALLS_REPO_TOKEN
 write-host "commit" $env:APPVEYOR_REPO_COMMIT
 write-host "message" $env:APPVEYOR_REPO_COMMIT_MESSAGE
@@ -8,6 +10,5 @@ write-host "job id" $env:APPVEYOR_JOB_ID
 write-host "branch" $env:APPVEYOR_REPO_BRANCH
 write-host "author email" $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL
 
-Get-ChildItem  *
-
-& $coveralls --opencover -i "./coverage.xml" --repoToken "MwCrQHSHjSubfBOeO2FFTxuO5bmdq1Aho" 
+& $reportgenerator -reports:"./coverage.xml" -targetdir:coverage -verbosity:Error
+& $coveralls --opencover -i "./coverage.xml" --repoToken "MwCrQHSHjSubfBOeO2FFTxuO5bmdq1Aho" --useRelativePaths
