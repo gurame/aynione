2b9aeb11a5004926908b8dcb18686a8789247976
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2017-10-09 00:19:29 -0500

    add

--- RunUnitTest.ps1
+++ RunUnitTest.ps1
@@ -5,4 +5,6 @@ write-host "repotoken" $env:COVERALLS_REPO_TOKEN
 write-host "commit" $env:APPVEYOR_REPO_COMMIT
 write-host "message" $env:APPVEYOR_REPO_COMMIT_MESSAGE
 write-host "job id" $env:APPVEYOR_JOB_ID
-& $coveralls --opencover -i coverage.xml --repoToken "MwCrQHSHjSubfBOeO2FFTxuO5bmdq1Aho" --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID  
+write-host "branch" $env:APPVEYOR_REPO_BRANCH
+write-host "author email" $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL
+& $coveralls --opencover -i "./coverage.xml" --repoToken "MwCrQHSHjSubfBOeO2FFTxuO5bmdq1Aho" --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID  
