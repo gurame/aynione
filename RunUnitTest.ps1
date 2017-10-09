9d69dec3eb159bb648ba08767a481dd866a8d790
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2017-10-09 00:15:44 -0500

    ad

--- RunUnitTest.ps1
+++ RunUnitTest.ps1
@@ -5,4 +5,4 @@ write-host "repotoken" $env:COVERALLS_REPO_TOKEN
 write-host "commit" $env:APPVEYOR_REPO_COMMIT
 write-host "message" $env:APPVEYOR_REPO_COMMIT_MESSAGE
 write-host "job id" $env:APPVEYOR_JOB_ID
-& $coveralls --opencover -i coverage.xml --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID  
+& $coveralls --opencover -i coverage.xml --repoToken "MwCrQHSHjSubfBOeO2FFTxuO5bmdq1Aho" --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID  
