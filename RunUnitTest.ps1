2f69527f8f951597cb39771db7dde8be7dec22da
Gregory Valderrama Vilca <gcvalderrama@hotmail.com>
2017-10-09 00:07:16 -0500

    add

--- RunUnitTest.ps1
+++ RunUnitTest.ps1
@@ -1,1 +1,4 @@
-dotnet test ./tests/aynione.unittest/aynione.unittest.csproj
+dotnet build -c  Debug
+OpenCover.Console.exe -target:".\dotnet.exe" -targetargs:"test ./tests/aynione.unittest/aynione.unittest.csproj" -register:user -filter:"+[aynione*]*" -oldStyle -output:./coverage.xml 
+$coveralls = (Resolve-Path "./Packages/coveralls.net.*/tools/csmacnz.coveralls.exe").ToString()
+& $coveralls --opencover -i coverage.xml --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID  
